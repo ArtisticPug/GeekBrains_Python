eed034714c9b11642c2eec11fbebb0647e095bb2
ArtisticPug <nemuritori@yandex.ru>
2020-09-17 00:58:39 +0300

    работа над ошибками 2 ???

--- GeekBrains_Python_Lesson_3/Lesson_3_Homework_2.py
+++ GeekBrains_Python_Lesson_3/Lesson_3_Homework_2.py
@@ -5,5 +5,7 @@ def my_func(**kwargs):
     for el in a:
         print(f"{el}: {a.get(el)}, ", end="")
 
+def second_func(name, lastname, year, city, email, phone):
+    print(f"Имя: {name}, Фамилия: {lastname}, Год рождения: {year}, Город проживания: {city}, Email: {email}, Номер телефона: {phone}")
 
-my_func()
+second_func((input("Укажите ваше имя: ")), (input("Укажите вашу фамилию: ")), (input("Укажите ваш год рождения: ")), (input("Укажите ваш город проживания: ")), (input("Укажите ваш Email: ")), (input("Укажите ваш номер телефона: ")))
