32dcc483d16eb79e060745b23c13d9f2e5bc67af
ArtisticPug <nemuritori@yandex.ru>
2020-09-20 21:57:48 +0300

    Update Lesson_4_Homework_3.py

--- GeekBrains_Python_Lesson_4/Lesson_4_Homework_3.py
+++ GeekBrains_Python_Lesson_4/Lesson_4_Homework_3.py
@@ -1,7 +1,7 @@
-new_list = [el for el in (list(range(20, 241))) if el % 20 == 0]
-print("=" * 20 + " Кратные 20 " + "=" * 20)
-print(new_list)
+# new_list = [el for el in (list(range(20, 241))) if el % 20 == 0]
+# print("=" * 20 + " Кратные 20 " + "=" * 20)
+# print(new_list)
 
-new_list2 = [el for el in (list(range(20, 241))) if el % 21 == 0]
-print("=" * 20 + " Кратные 21 " + "=" * 20)
+new_list2 = [el for el in (list(range(20, 241))) if el % 21 == 0 or el % 20 == 0]
+print("=" * 20 + " Кратные 20 или 21 " + "=" * 20)
 print(new_list2)
